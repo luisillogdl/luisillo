f97a7c34d525c60b72ea0bc7056f7831184f00ac
LUIS LV <juanluigi@gmail.com>
2015-08-29 15:31:27 -0500

    creado el proyecto y agregado estatus al mismo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Luis::Application.config.secret_token = '3b60d6b75aa48f621e7a5b41fb25fc8c7dacb37331c419c463decb1db3e5fa01909d57edb68b48eeb2d5a5759b95585bd0628a6c4980fbef390e0fc497da8ca4'
